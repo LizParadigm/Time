3f6a2349927087d51aa9e8679d497712b919e6ab
jesss635000@gmail.com <jesss635000@gmail.com>
2024-04-02 23:44:23 -0600

    repositorio ordenado

---
+++ funtions/ventanas-emergentes/create-alarma.js
@@ -0,0 +1,10 @@
+// Función para mostrar la ventana emergente
+function ventanaCrearAlarma() {
+  document.getElementById("crearNuevaAlarma").style.display = "block";
+}
+
+// Función para cerrar la ventana emergente
+function cerrarVentanaCrear() {
+  document.getElementById("crearNuevaAlarma").style.display = "none";
+}
+
---
+++ funtions/ventanas-emergentes/delete-confirmar.js
@@ -0,0 +1,11 @@
+// Función para mostrar la ventana emergente
+function ventanaEliminarAlarma() {
+  document.getElementById("eliminarAlarma").style.display = "block";
+}
+
+// Función para cerrar la ventana emergente
+function cerrarVentanaDelete() {
+  document.getElementById("eliminarAlarma").style.display = "none";
+}
+
+
